88937bf117ada787604e85aee24929ded05cc638
bilalelou <bilal.12elou@gmail.com>
2024-11-08 12:58:46 +0100

    add new start

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ServerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/',[ServerController::class,'index'])->name(server.index);
