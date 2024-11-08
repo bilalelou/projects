4f5972e9977fb172e25c18d401e8e1f31beb642d
bilalelou <bilal.12elou@gmail.com>
2024-11-08 15:33:04 +0100

    change

--- resources/views/servers/index.blade.php
+++ resources/views/servers/index.blade.php
@@ -1,1 +1,15 @@
-show all servers
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+</head>
+<body>
+    navbar
+    siderbar
+    servers
+    footer
+</body>
+</html>
